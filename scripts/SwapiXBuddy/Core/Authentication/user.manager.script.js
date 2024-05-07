5df95f8cc98943a7beb856dd4ec53545a8da4655
JWLMT88 <120219149+JWLMT88@users.noreply.github.com>
2024-05-07 14:22:15 +0200

    user manager new

--- scripts/SwapiXBuddy/Core/Authentication/user.manager.script.js
+++ scripts/SwapiXBuddy/Core/Authentication/user.manager.script.js
@@ -1,3 +1,22 @@
+/*
+   ------------------------------------------------------------------------------
+   Copyright (c) 2024 JW Limited. All rights reserved.
+
+   Project: SwapiX 
+   Module: Web Client 
+   File: user.manager.script.js
+   Company: JW Limited (licensed)
+   Author: Joey West (CEO)
+
+   This software is proprietary to JW Limited and constitutes valuable 
+   intellectual property. It is entrusted solely to employees named above
+   and may not be disclosed, copied, reproduced, transmitted, or used in 
+   any manner outside of the scope of its license without prior written
+   authorization from JW Limited.
+
+   ------------------------------------------------------------------------------
+*/
+
 class UserManager
 {
     constructor()
@@ -9,7 +28,7 @@ class UserManager
             this.qrMng = new QueryManager();
             this.instance = this;
             this.apiURI = "https://mutual-loved-filly.ngrok-free.app/api/";
-            this.requestURL =  apiURI + "v1/";
+            this.requestURL =  this.apiURI + "v1/";
         }
         
         return this.instance;
